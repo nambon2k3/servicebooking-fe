9edd17f599001f17d414be93f7e7c848e06d66fa
Hoai Nam <nvhnam.work@gmail.com>
2025-06-08 18:26:39 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://3.107.57.226/v1/',
+    apiUrl: 'http://3.107.57.226:8080/v1/',
     // apiUrl: 'http://sava-api-env.eba-r8y69kwm.ap-southeast-1.elasticbeanstalk.com/api',
 };
